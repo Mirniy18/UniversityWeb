31cd1051c570cab47179c64f69e0ef6823842f50
Stas <stasstas1807@gmail.com>
2020-11-27 13:42:41 +0200

    SQL injection protection

--- pr9/auth.php
+++ pr9/auth.php
@@ -1,16 +1,21 @@
 <?php
 if (array_key_exists('login', $_POST)) {
 	session_start();
-	$_SESSION['login'] = $_POST['login'];
-	$_SESSION['password'] = $_POST['password'];
-
-	include('db.php');
-
-	if (hasUser($_SESSION['login'], $_SESSION['password'])) {
-		$_SESSION['auth'] = true;
-		header('location: restricted.php');
-	} else {
-		echo 'Invalid login or password';
+	require_once 'db.php';
+	if (count($_POST) > 0) {
+		$stmt = $conn->prepare('SELECT * FROM users WHERE login = ? and password = ?;');
+		$stmt->bind_param('ss', $_POST['login'], $_POST['password']);
+		$stmt->execute();
+		$res = $stmt->get_result();
+		$row = $res->fetch_assoc();
+		if (is_array($row)) {
+			$_SESSION['login'] = $row['login'];
+			$_SESSION['password'] = $row['password'];
+			$_SESSION['auth'] = true;
+			header('location: restricted.php');
+		} else {
+			echo 'Invalid login or password';
+		}
 	}
 } else {
 	echo 'Access restricted.';
--- pr9/db.php
+++ pr9/db.php
@@ -9,25 +9,3 @@ $conn = new mysqli($servername, $username, $password, $database);
 if ($conn->connect_error) {
 	die('Connection to DB failed: ' . $conn->connect_error);
 }
-
-function getUsers()
-{
-	global $conn;
-	$sql = 'SELECT id, login, password FROM users';
-	return $conn->query($sql);
-}
-
-function hasUser($login, $password)
-{
-	$users = getUsers();
-
-	if ($users->num_rows > 0) {
-		while ($row = $users->fetch_assoc()) {
-			if ($login == $row['login']) {
-				return $password == $row['password'];
-			}
-		}
-
-		return false;
-	}
-}
