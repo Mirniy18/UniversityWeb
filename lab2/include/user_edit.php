b610ba06c323424d2cc9bf46a75820fe7f5b7f21
Stas <stasstas1807@gmail.com>
2020-11-29 20:08:35 +0200

    no changing role for users

--- lab2/include/user_edit.php
+++ lab2/include/user_edit.php
@@ -6,14 +6,28 @@ if (count($_POST) == 0 || !array_key_exists('login', $_POST)) {
 
 require_once 'include/db.php';
 if ($_SESSION['id'] == $_GET['id'] || $_SESSION['id_role'] == 1) {
-	if ($_POST['edit']) {
-		$stmt = $conn->prepare('UPDATE users SET first_name = ?, last_name = ?, password = ?, login = ?, id_role = ? WHERE id = ?;');
-		$stmt->bind_param('ssssii', $_POST['first_name'], $_POST['last_name'], $_POST['password'], $_POST['login'], $_POST['id_role'], $_GET['id']);
+	if (array_key_exists('edit', $_POST)) {
+		if ($_POST['password'] != $_POST['password2']) {
+			echo 'Passwords do not match.';
+			exit();
+		}
+
+		if ($_SESSION['id_role'] == 1) {
+			$stmt = $conn->prepare('UPDATE users SET first_name = ?, last_name = ?, password = ?, login = ?, id_role = ? WHERE id = ?;');
+			$stmt->bind_param('ssssii', $_POST['first_name'], $_POST['last_name'], $_POST['password'], $_POST['login'], $_POST['id_role'], $_GET['id']);	
+		} else {
+			$stmt = $conn->prepare('UPDATE users SET first_name = ?, last_name = ?, password = ?, login = ? WHERE id = ?;');
+			$stmt->bind_param('ssssi', $_POST['first_name'], $_POST['last_name'], $_POST['password'], $_POST['login'], $_GET['id']);
+		}
 		if (!$stmt->execute()) {
 			echo 'Database error: ';
 			echo mysqli_error($conn);
 		}
-	} elseif ($_POST['delete']) {
+	} elseif (array_key_exists('delete', $_POST)) {
 		$conn->query('DELETE FROM users WHERE id = ' . $_GET['id']);
+
+		if ($_SESSION['id'] == $_GET['id']) {
+			header('location: sign_out.php');
+		}
 	}
 }
