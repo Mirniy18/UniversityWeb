4c4642b57550331aee8ca393eccf22bea4aba0eb
Stas <stasstas1807@gmail.com>
2020-11-29 20:08:13 +0200

    fixed registration

--- lab2/include/register.php
+++ lab2/include/register.php
@@ -9,13 +9,18 @@ require_once 'db.php';
 if (count($_POST) > 0) {
 	session_start();
 
-	if ($_POST['add'] && !(array_key_exists('id_role', $_SESSION) && $_SESSION['id_role'] == 1)) {
+	if (array_key_exists('add', $_POST) && !(array_key_exists('id_role', $_SESSION) && $_SESSION['id_role'] == 1)) {
 		echo 'Access restricted.';
 		exit();
 	}
 
-	$sql = 'SELECT users.id, users.first_name, users.last_name, users.login, roles.title FROM users LEFT JOIN roles ON users.id_role = roles.id';
-	$users = $conn->query($sql);
+	if ($_POST['password'] != $_POST['password2']) {
+		echo 'Passwords do not match.';
+		exit();
+	}
+
+	$stmt = $conn->prepare('INSERT INTO users (first_name, last_name, password, login, id_role) VALUES (?, ?, ?, ?, ?);');
+	$stmt->bind_param('sssss', $_POST['first_name'], $_POST['last_name'], $_POST['password'], $_POST['login'], $_POST['id_role']);
 
 	if ($stmt->execute()) {
 		if ($_POST['sign_up']) {
