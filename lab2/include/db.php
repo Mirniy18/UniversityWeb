d1dd12cf4042a4b24f7c28c3b141286ede24f757
Stas <stasstas1807@gmail.com>
2020-11-29 20:07:21 +0200

    passwords for admins

--- lab2/include/db.php
+++ lab2/include/db.php
@@ -1,6 +1,4 @@
 <?php
-
-
 $servername = 'localhost';
 $username = 'root';
 $password = '';
@@ -14,11 +12,26 @@ if ($conn->connect_error) {
 
 function createTable() {
 	global $conn;
-	$sql = 'SELECT users.id, users.first_name, users.last_name, users.login, roles.title FROM users LEFT JOIN roles ON users.id_role = roles.id';
+
+	$admin = array_key_exists('id_role', $_SESSION) && $_SESSION['id_role'] == 1;
+
+	if ($admin) {
+		$sql = 'SELECT users.id, users.first_name, users.last_name, users.login, users.password, roles.title FROM users LEFT JOIN roles ON users.id_role = roles.id';
+	} else {
+		$sql = 'SELECT users.id, users.first_name, users.last_name, users.login, roles.title FROM users LEFT JOIN roles ON users.id_role = roles.id';
+	}
+
 	$users = $conn->query($sql);
 
 	if ($users->num_rows > 0) {
-		echo '<tr><th>Id</th><th>First Name</th><th>Last Name</th><th>Login</th><th>Role</th></tr>';
+		echo '<tr><th>Id</th><th>First Name</th><th>Last Name</th><th>Login</th><th>Role</th>';
+
+		if ($admin) {
+			echo '<th>Password</th></tr>';
+		} else {
+			echo '</tr>';
+		}
+
 		while ($user = $users->fetch_assoc()) {
 			echo '<tr>';
 			echo '<td><a href="user_page.php?id=' . $user['id'] . '">' . $user['id'] . '</a></td>';
@@ -26,6 +39,9 @@ function createTable() {
 			echo '<td>' . $user['last_name'] . '</td>';
 			echo '<td>' . $user['login'] . '</td>';
 			echo '<td>' . $user['title'] . '</td>';
+			if ($admin) {
+				echo '<td>' . $user['password'] . '</td>';
+			}
 			echo '</tr>';
 		}
 	}
