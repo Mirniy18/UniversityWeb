a4b1e1cdde32475f674dd87bec8e7c576559cbf3
Stas <stasstas1807@gmail.com>
2020-11-29 23:27:49 +0200

    form verification

--- lab2/include/login_modal.php
+++ lab2/include/login_modal.php
@@ -2,11 +2,11 @@
 	<form action="sign_in.php" method="POST">
 		<div class="modal-content">
 			<div class="input-field col s3">
-				<input id="login" name="login" type="text" class="validate">
+				<input id="login" name="login" type="text" class="validate" required>
 				<label for="login">Login</label>
 			</div>
 			<div class="input-field col s3">
-				<input id="password" name="password" type="password" class="validate" minlength="6">
+				<input id="password" name="password" type="password" class="validate" minlength="6" required>
 				<label for="password">Password</label>
 			</div>
 		</div>
